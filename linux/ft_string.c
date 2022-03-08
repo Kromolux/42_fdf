e11cb72267a301f3a615c6610286696059015193
Rene Kaufmann <rkaufman@student.42wolfsburg.de>
2022-03-08 11:41:00 +0100

    changes to linux

--- linux/ft_string.c
+++ linux/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: rkaufman <rkaufman@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 11:53:48 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/03/08 11:00:43 by rkaufman         ###   ########.fr       */
+/*   Updated: 2022/03/08 11:40:09 by rkaufman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,10 +59,8 @@ void	ft_check_input_exit_on_error(t_map *screen, char **input)
 int	ft_valid_number(char *argv)
 {
 	int		i;
-	//int		len;
 
 	i = 0;
-	//len = ft_strlen(argv);
 	while (argv[i])
 	{
 		if (argv[i] < '0' || argv[i] > '9')
